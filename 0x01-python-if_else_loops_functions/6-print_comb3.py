e3f3074063bc4ea0421324fcce06badc712fb565
Roylouis-web <powellleroy488@gmail.com>
2022-11-29 20:58:16 +0000

    MY seventh task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
-        if i > j or i == j:
-            continue
-        elif i < 8:
-            print(f"{i}{j}, ", end='')
-        else:
-            print(f"{i}{j}\n")
+        for j in range(10):
+                if i > j or i == j:
+                        continue
+                elif i < 8:
+                        print("{:d}{:d}, ".format(i, j), end='')
+                else:
+                        print("{:d}{:d}".format(i, j))
