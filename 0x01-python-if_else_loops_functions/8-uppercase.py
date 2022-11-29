e4e92b287d264681ded7047ddf3577af5e5ef5d1
Roylouis-web <powellleroy488@gmail.com>
2022-11-29 21:16:53 +0000

    My 9th task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,7 @@ def uppercase(str):
     out = ''
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            l = ord(i) - 32
-            out += chr(l)
+            out += chr(ord(i) - 32)
         else:
             out += i
     print("{:s}".format(out))
