22111817d52f7a6c52ddc004cd2c7fc84058b09e
Roylouis-web <powellleroy488@gmail.com>
2022-11-29 20:42:37 +0000

    My sixth task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print(f"{i // 10}{i % 10}, ", end='')
+        print("{:d}{:d}, ".format(i // 10, i % 10), end='')
     else:
-        print(f"{i // 10}{i % 10}\n")
+        print("{:d}{:d}\n".format(i // 10, i % 10))
