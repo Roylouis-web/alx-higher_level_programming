3c2b047c49ee1c515c0fc711e77709e0793c603c
Roylouis-web <powellleroy488@gmail.com>
2022-12-01 19:02:46 +0000

    corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,13 @@ if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv) - 1
 
-    if argc > 0:
+    if argc > 1:
         print("{:d} arguments:".format(argc))
         for i in range(1, argc + 1):
             print("{:d}: {:s}".format(i, argv[i]))
-    else:
+    elif argc == 1:
+        print("{:d} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{:d}: {:s}".format(i, argv[i]))
+    elif argc == 0:
         print("{:d} arguments.".format(argc))
