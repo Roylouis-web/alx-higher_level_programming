21399600f2c363f19fd14693cc003ca264648844
Roylouis-web <powellleroy488@gmail.com>
2022-12-07 23:56:35 +0000

    Corrections made

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    max = 0
 
-    b_score = max(a_dictionary.values(), default=None)
-    for k, v in a_dictionary.items():
-        if v == b_score:
-            return k
+    for i in a_dictionary:
+        if a_dictionary[i] > max:
+            max = a_dictionary[i]
+
+    for i in a_dictionary:
+        if a_dictionary[i] == max:
+            return i
