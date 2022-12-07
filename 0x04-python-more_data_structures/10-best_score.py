61ed09b5f665ace96ba68cacd9599433ee2f5002
Roylouis-web <powellleroy488@gmail.com>
2022-12-07 23:28:41 +0000

    My advance tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
@@ -5,5 +6,4 @@ def best_score(a_dictionary):
     b_score = max(a_dictionary.values(), default=None)
     for k, v in a_dictionary.items():
         if v == b_score:
-            return k
-
+            return 
