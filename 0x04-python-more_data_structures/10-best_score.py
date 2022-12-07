a8c8656786d254001b4de379c8c68020ff922b2c
Roylouis-web <powellleroy488@gmail.com>
2022-12-07 23:52:10 +0000

    Corrections made

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     str = ''
-    max = 0
-
+    max = max(a_dictionary.values(), default=None)
     for i in a_dictionary:
         if a_dictionary[i] > max:
             max = a_dictionary[i]
