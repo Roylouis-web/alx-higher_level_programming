c6228b7ac134c33b1573cc95a8e3cc8334b2fcd9
Roylouis-web <powellleroy488@gmail.com>
2023-01-10 03:30:12 +0000

    corrections made

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,6 +21,6 @@ class BaseGeometry():
         """validates integer input
         """
         if type(value) != int:
-            raise TypeError(name + ' must be an integer')
+            raise TypeError(name + ' ' + "must be an integer")
         elif value <= 0:
-            raise ValueError(name + 'must be greater than 0')
+            raise ValueError(name + ' ' + "must be greater than 0")
