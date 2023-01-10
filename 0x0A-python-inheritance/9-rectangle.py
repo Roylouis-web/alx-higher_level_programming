0bc2de8b57f3e348b18a27d587c496d55f1325dd
Roylouis-web <powellleroy488@gmail.com>
2023-01-10 04:08:58 +0000

    corrections made

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+    A non empty class called BaseGeometry
+    that inherits from a super class Object
+    and raises an error when area method is
+    called and also validates the parameter
+    'value'
+"""
 
-"""
-    class Rectangle that inherits from BaseGeometry
-    (7-base_geometry.py)
-"""
+
+class BaseGeometry():
+    """for use with shapes. Super class.
+    """
+
+    def area(self):
+        """instance method to calculate area of shape
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates integer input
+        """
+        if type(value) != int:
+            raise TypeError(name + ' ' + "must be an integer")
+        elif value <= 0:
+            raise ValueError(name + ' ' + "must be greater than 0")
+
 
 class Rectangle(BaseGeometry):
     """
@@ -19,7 +39,7 @@ class Rectangle(BaseGeometry):
         """
         self.__width = width
         self.__height = height
-        super().integer_validator("width", self.__height)
+        super().integer_validator("width", self.__width)
         super().integer_validator("height", self.__height)
 
     def area(self):
