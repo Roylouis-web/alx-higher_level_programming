95f29956b279e1df62b7a30ce839779d2f12841c
Roylouis-web <powellleroy488@gmail.com>
2023-01-10 01:42:15 +0000

    Correction to file 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,7 +3,7 @@
     class Rectangle that inherits from BaseGeometry
     (7-base_geometry.py)
 """
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
