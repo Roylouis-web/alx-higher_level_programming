5508aada3b583e94f2317d23c1cdf346f9d49689
Roylouis-web <powellleroy488@gmail.com>
2023-01-11 03:53:12 +0000

    My first six tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,9 @@
 
 
 def read_file(filename=""):
-    """reads a file
+    """reads a text file (UTF-8)
+        and prints it to stdout
     """
-    with open(filename, encoding='utf-8') as myFile:
+    with open(filename, mode= 'r', encoding='utf-8') as myFile:
         for lines in myFile:
             print(lines, end='')
