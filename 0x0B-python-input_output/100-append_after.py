bfa210afe73e0173c795d4a9091fb609e057e293
Roylouis-web <powellleroy488@gmail.com>
2023-01-11 05:10:26 +0000

    My first six tasks

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+a function that inserts a 
+line of text to a file, after
+each line containing specific
+string
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
