c7aba0407f3acd5bb298f46278d4f41950b4459b
Roylouis-web <powellleroy488@gmail.com>
2023-01-11 04:14:52 +0000

    corrections

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,10 +6,7 @@ def read_file(filename=""):
         and prints it to stdout
     """
 
-<<<<<<< HEAD
-    with open(filename, mode='r', encoding='utf-8') as myFile:
-=======
     with open(filename, mode= 'r', encoding='utf-8') as myFile:
->>>>>>> c03e75aa838a436cd05cb552c09c1bd864824534
+
         for lines in myFile:
             print(lines, end='')
