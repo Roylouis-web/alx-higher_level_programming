58e07063033028a6756f09f92ac517ce4a5294b1
Roylouis-web <powellleroy488@gmail.com>
2023-01-11 03:51:00 +0000

    My first six tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@ def read_file(filename=""):
     """reads a file
     """
     with open(filename, encoding='utf-8') as myFile:
-        print(myFile.read(), end='')
+        for lines in myFile:
+            print(lines, end='')
