54e2091ea76f112597bc865a95d5c253c4e147ff
Roylouis-web <powellleroy488@gmail.com>
2023-01-11 04:44:23 +0000

    My first six tasks

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-
-
 import json
+"""
+    a function that returns the JSON
+    representation of an object (string):
+"""
 
 
 def to_json_string(my_obj):
-    """returs json string containing object's representation
-        -> handles no exceptions in serialization proccess
     """
+    param my_obj: object parameter
+    return: the JSON
+    representation of an object (string)
+    """
+
     return json.dumps(my_obj)
