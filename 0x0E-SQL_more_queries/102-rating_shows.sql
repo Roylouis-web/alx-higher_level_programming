2632f115074f7c439c01aa0c2edeca82f82628a6
Roylouis-web <powellleroy488@gmail.com>
2023-02-16 02:19:43 +0000

    Corrections made

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,8 @@
--- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- Records are ordered by descending rating.
-SELECT `name`, SUM(`rate`) AS `rating`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON s.`genre_id` = g.`id`
-
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
        INNER JOIN `tv_show_ratings` AS r
-       ON r.`show_id` = s.`show_id`
- GROUP BY `name`
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
  ORDER BY `rating` DESC;
