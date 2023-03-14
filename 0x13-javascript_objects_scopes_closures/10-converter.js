3be642ce6f226ae226b3666c49ff78f905c4b096
Roylouis-web <powellleroy488@gmail.com>
2023-03-14 12:39:09 +0000

    Corrections made

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,38 +1,9 @@
 #!/usr/bin/node
-function rev (num) {
-  let str = '';
-  while (parseInt(num) > 0) {
-    str += parseInt(num % 10);
-    num /= 10;
+exports.converter = function converter (base) {
+  if (typeof converter.Num === 'undefined') {
+    converter.Num = base;
+    return converter;
+  } else {
+    return parseInt(converter.Num, base);
   }
-
-  return parseInt(str);
-}
-
-exports.converter = function (base) {
-  return function myConverter (num) {
-    let str = '';
-
-    if (base === 16) {
-      if (num === 10) {
-        return 'a';
-      } else if (num === 11) {
-        return 'b';
-      } else if (num === 12) {
-        return 'c';
-      } else if (num === 13) {
-        return 'd';
-      } else if (num === 14) {
-        return 'e';
-      } else if (num === 15) {
-        return 'f';
-      }
-    }
-
-    while (parseInt(num) > 0) {
-      str += parseInt(num % 10);
-      num /= base;
-    }
-    return (rev(parseInt(str)));
-  };
 };
