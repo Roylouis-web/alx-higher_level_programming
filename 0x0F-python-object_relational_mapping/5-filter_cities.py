a1f7ec4c6952925e0e1de5f6aaac9b62779855e9
Roylouis-web <powellleroy488@gmail.com>
2023-03-18 05:42:00 +0000

    Some of my tasks

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,6 +12,9 @@ import MySQLdb
 
 if __name__ == '__main__':
     argv = sys.argv
+    if len(argv) < 4:
+        print("Usage: {} username password database_name".format(argv[0]))
+        exit(1)
     user_name = argv[1]
     password = argv[2]
     db_name = argv[3]
