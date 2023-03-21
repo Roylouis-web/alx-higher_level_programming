027686e135e0c1eaf6a726a03ea75e8e37e464bc
Roylouis-web <powellleroy488@gmail.com>
2023-03-21 19:33:06 +0000

    Corrections made

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
 """
-import sys
+     a script that lists all State objects, and
+     corresponding City objects, contained in the database hbtn_0e_101_usa
+"""
+
+
+from sqlalchemy import create_engine
 from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
-from sqlalchemy.orm import relationship
+import sys
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+if __name__ == '__main__':
+    engine = create_engine("sqlite:///hbtn_0e_101_usa.db")
     Base.metadata.create_all(engine)
     session = Session(engine)
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
-        for city_ins in instance.cities:
-            print("    ", end="")
-            print(city_ins.id, city_ins.name, sep=": ")
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
